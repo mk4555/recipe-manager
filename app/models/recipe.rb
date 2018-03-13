8be182f042b244477910f201c40f6ba6babe3a3c
github username <github email address>
2018-03-13 18:44:26 -0400

    added scope methods for recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,6 @@ class Recipe < ActiveRecord::Base
   belongs_to :user
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
+  scope :highest_rating, -> {order(rating: :desc)}
+  scope :lowest_rating, -> {order(rating: :asc)}
 end
