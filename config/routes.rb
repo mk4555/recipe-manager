ade3207bda4e22d2ac9eda3cdf95999969250c25
github username <github email address>
2018-03-16 00:02:25 -0400

    added user routes and nested recipe routes for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :paths => 'users'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'recipes#index'
+
+  resources :users do
+    resources :recipes
+  end
+
   resources :recipes
   resources :ingredients
   # resources :users, only: [:index, :show]
