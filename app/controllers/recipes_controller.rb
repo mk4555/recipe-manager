6844f7d913e26d208e33811263f20ca9956f100b
github username <github email address>
2018-03-15 22:39:08 -0400

    added recipe edit and delete action

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -21,6 +21,26 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find_by_id(params[:id])
   end
 
+  def edit
+    @recipe = Recipe.new
+  end
+
+  def update
+    @recipe = Recipe.update(recipe_params)
+    if @recipe.save
+      # have flash message indicating it was updated successfully
+      redirect_to recipe_path(@recipe)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.destroy
+    redirect_to recipes_path
+  end
+
   private
   # need to add ingredients id as a parameter
   def recipe_params
