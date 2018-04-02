1133c8052e786b6ff2a83f6cb1442d4e7e8d11d9
github username <github email address>
2018-04-02 17:31:42 -0400

    quality validation added

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -1,7 +1,7 @@
 class RecipeIngredient < ActiveRecord::Base
   belongs_to :recipe
   belongs_to :ingredient
-
+  validates :quantity, presence: true
   def capitalized_ingredient
     ingredient.name.capitalize
   end
