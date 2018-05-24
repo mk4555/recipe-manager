26c836fcf1e29daf2f9e91ed39238b40eab0ed7d
github username <github email address>
2018-05-24 12:44:13 -0400

    took out links to recipe show page on show page and updated next recipe button to load next id

--- app/assets/javascripts/recipe.js
+++ app/assets/javascripts/recipe.js
@@ -20,12 +20,12 @@ $(function () {
     var nextId = parseInt($(".js-next-recipe").attr("data-id")) + 1;
     $.get("/recipes/" + nextId + ".json", function(recipe) {
       console.log(recipe)
-      $("#name").html(`<h4><a href='/recipes/${recipe["id"]}'> ${recipe["name"].charAt(0).toUpperCase() + recipe["name"].slice(1)}</a></h4>`)
+      $("#name").html(`<h4> ${recipe["name"].charAt(0).toUpperCase() + recipe["name"].slice(1)}</h4>`)
       $(".rating").html(`Rating: ${recipe["rating"]}/5`);
       $(".email").html(`Recipe by: <a href='/users/${recipe["user"]["id"]}/recipes'>${recipe["user"]["email"]}</a>`);
       $(".cookTime").html(`Cook Time: ${recipe["cook_time"]}`);
       $(".description").html(`Description: ${recipe["description"]}`);
-      $(".js-next").attr("data-id", recipe["id"]);
+      $(".js-next-recipe").attr("data-id", recipe["id"]);
     });
   });
 });
