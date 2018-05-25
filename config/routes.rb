1b53ae5ff16ec9b72c6c0956b80ad6b57ac82a62
github username <github email address>
2018-05-24 20:13:11 -0400

    routes added for review

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'recipes#index'
 
-  resources :directions
-  
+  resources :directions, [:new, :create]
+  resources :reviews, only: [:new, :create]
   resources :users do
     resources :recipes
   end
